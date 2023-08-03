4425ab3230944ce8f240fbfcdb650a4793c9a502
trigger <NULL>
2023-08-03 11:45:59 +0800

    second commit

---
+++ SocketTrainig/server.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socket_Server
+{
+    internal class server
+    {
+        static void Main(string[] args)
+        {   //创建socket实例
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            //用户输入ip地址和端口号
+            Console.WriteLine("请输入服务端IP：\n");
+            IPAddress ipAddress = IPAddress.Parse(Console.ReadLine());
+            Console.WriteLine("请输入服务端Port：\n");
+            int Port = int.Parse(Console.ReadLine());
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, Port);
+
+
+            //socket绑定ip地址和端口号
+            socket.Bind(localEndPoint);
+
+
+            Console.WriteLine("正在等待链接...");
+            socket.Listen(10);
+
+            int couter = 0;
+            while (true)
+            {
+                // 接受连接
+                Socket handler = socket.Accept();
+                if(couter<1)
+                Console.WriteLine($"Connected: {handler.RemoteEndPoint}");
+
+                couter++;
+
+                // 读取数据
+                byte[] buffer = new byte[1024];
+                int bytesRec = handler.Receive(buffer);
+                string data = Encoding.ASCII.GetString(buffer, 0, bytesRec);
+                Console.WriteLine($"Received: {data}");
+
+                // 发送数据
+                    byte[] msg = Encoding.ASCII.GetBytes(Console.ReadLine());
+                    handler.Send(msg);
+              
+                // 关闭连接
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+    }
+}
---
+++ Socket_Client/client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Socket_Client
+{
+    internal class client
+    {
+        static void Main(string[] args)
+        {
+            // 创建一个Socket并连接到服务器
+            Console.WriteLine("请输入目标端IP地址");
+            IPAddress ipAddress = IPAddress.Parse(Console.ReadLine());
+            Console.WriteLine("请输入目标端端端口号");
+            int Port = int.Parse(Console.ReadLine());
+            
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, Port);
+
+            while (true)
+            {
+                Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                sender.Connect(remoteEP);
+
+            
+            // 发送数据
+            
+                Console.WriteLine("向服务端发送数据，发送“quit！退出”：\n");
+                string message = Console.ReadLine();
+                byte[] msg = Encoding.ASCII.GetBytes(message);
+                int bytesSent = sender.Send(msg);
+                Console.WriteLine($"Sent: {message}");
+                if (message == "quit!")
+                {
+                    break;
+                }
+
+                // 接受数据
+                byte[] buffer = new byte[1024];
+                int bytesRec = sender.Receive(buffer);
+                string data = Encoding.ASCII.GetString(buffer, 0, bytesRec);
+                Console.WriteLine($"Received: {data}");
+
+                // 关闭连接
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+
+                Console.ReadKey();
+        }
+    }
+}
